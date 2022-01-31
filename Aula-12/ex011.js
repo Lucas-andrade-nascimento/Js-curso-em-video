4a26eb87a97ae9f56e898690dde00589c4c21e1c
Lucas Andrade <andradedesa.lucas@gmail.com>
2022-01-31 15:51:34 -0300

    update 31-01

---
+++ Aula-12/ex011.js
@@ -0,0 +1,13 @@
+var idade = 70
+console.log(`Você tem ${idade} anos.`)
+if (idade < 16 ) {
+    console.log('Nao vota')
+
+}
+else if (idade <18 || idade >65) {
+        console.log('Voto facultativo')
+    }
+ 
+else {
+        console.log('voto obrigatorio')
+}
